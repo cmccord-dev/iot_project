dbeaced6728c1bc357bd87605199796003530e8e
eiowlta <cassandra.l.mccord@gmail.com>
2017-05-16 17:25:52 +0300

    added ws to gateway

--- gateway/main.js
+++ gateway/main.js
@@ -1,6 +1,8 @@
 var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 var stream = require("stream");
 var readline = require("readline");
+var WebSocket = require('ws');
+ 
 
 
 
@@ -17,36 +19,40 @@ btSerial.on('found', function(address, name) {
 				btinStream.push(buffer.toString("utf-8"));
 			});
 			
-			
-			
-			
-			
-			stdinLines.on("line", function(data){
-				//received data from console (later server)
-				
-				
-				
-				
-				//don't mess with this!
-				btSerial.write(new Buffer(data, "utf-8"), function(err, bytes){
-					if(bytes != data.length){
-						console.error("did not write all the data...");
-					}
-					if(err){
-						console.error(err);
-					}
+			var ws = new WebSocket('ws://turrisserver.ddns.net:9191/');
+
+			//var ws = {on:function(){}};
+			ws.on('open', function open() {
+				ws.on('message', function incoming(data, flags) {
+					//received data from console (later server)
+					
+					
+					
+					
+					//don't mess with this!
+					btSerial.write(new Buffer(data, "utf-8"), function(err, bytes){
+						if(bytes != data.length){
+							console.error("did not write all the data...");
+						}
+						if(err){
+							console.error(err);
+						}
+					});
 				});
-			});
-			btinLines.on("line", function(data){
-				//message from arduino
 				
 				
+				btinLines.on("line", function(data){
+					//message from arduino
+					console.log("from arduino: ", data)
+					ws.send(data.toString("utf-8"));
+					
+					
+					
+					//console.log(data);
+				});
 				
-				
-				console.log(data);
-			});
 			
-			
+			});
 			
 			
 			
@@ -54,7 +60,7 @@ btSerial.on('found', function(address, name) {
 			
 			
 		}, function () {
-			console.error('cannot connect');
+			console.error(address, 'cannot connect');
 		});
 
 		// close the connection when you're ready
